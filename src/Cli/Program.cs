c818033812ef0ec941c4264688e2d18e03052e46
Artem Fedoniuk web2025-1 <iiilssofff@gmail.com>
2026-09-15 21:56:03 +0300

    lab01: solution CrossApp, Cli, вибір домену

---
+++ src/Cli/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+// Налаштування UTF-8 для правильного виводу кирилиці в консолі Windows
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+Console.WriteLine("CrossApp - практикум з крос-платформного програмування");
+Console.WriteLine("Студент: Федонюк Артем, група ФЕІ-34");
+Console.WriteLine(new string('-', 55));
+Console.WriteLine($"ОС (OSDescription): {RuntimeInformation.OSDescription}");
+Console.WriteLine($"ОС (Environment) : {Environment.OSVersion}");
+Console.WriteLine($"Архітектура процесу: {RuntimeInformation.ProcessArchitecture}");
+Console.WriteLine($"Версія .NET (CLR): {Environment.Version}");
+Console.WriteLine($"Runtime            : {RuntimeInformation.FrameworkDescription}");
+Console.WriteLine($"Каталог застосунку: {AppContext.BaseDirectory}");
+Console.WriteLine($"Поточний каталог  : {Environment.CurrentDirectory}");
+Console.WriteLine(new string('-', 55));
+Console.WriteLine("Предметна область: Бібліотека (Book, BookCopy, Reader, Loan)");
